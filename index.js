c6ede44af2233a423fdbd364f819b1716f7f2dbc
Obinna Onuigbo <onuigboobinna@gmail.com>
2023-11-02 02:55:05 +0100

    Second commit: Currency converter working

--- index.js
+++ index.js
@@ -8,24 +8,32 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 //Configure axios to retry failed requests up to three times before sending error.
 axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
+//Declare an empty data object on a global scope so it is available to all other route handlers.
+let content;
 
 app.get("/", async (req, res) => {
     try {
-        const response = await axios.get("https://www.frankfurter.app/currencies");
-        console.log(response.data);
-        const currencies = Object.keys(response.data);
-        const currencyName = Object.values(response.data);
-        const data = {
-            content: {
-                currency: currencies,
-                currencyName: currencyName,
-            }
-        }        
-        res.render("index.ejs", data);
+        //Get the all the API supported currencies
+        const axiosResponse = await axios.get("https://www.frankfurter.app/currencies");
+        //Get the latest prices from API. Prices use base currency of Euro.
+        const latestPrices = await axios.get("https://www.frankfurter.app/latest")
+        //Store price rates only;
+        const prices = latestPrices.data.rates;
+        //Convert 'axiosResponse.data' object keys to an array so we can iterate over it for our EJS options.  
+        const currencies = Object.keys(axiosResponse.data);
+        //Convert 'axiosResponse.data' object values to an array for our EJS options content.
+        const currencyName = Object.values(axiosResponse.data);
+        content = {
+            currency: currencies,
+            currencyName: currencyName,
+            prices: prices,
+        }
+
+        res.render("index.ejs", { content: content, });
     } catch (error) {
-        if(error.response) {
+        if (error.response) {
             console.error(error.response.data)
-        } else if(error.request){
+        } else if (error.request) {
             console.log("Request not sent: ")
         } else {
             console.error(error.message);
@@ -35,8 +43,48 @@ app.get("/", async (req, res) => {
 
 })
 
-app.get("/chart", (req, res)=>{
-    res.render("index.ejs");
+app.post("/converter", async (req, res) => {
+
+    console.log("request body: ", req.body);
+    const amount = req.body.amount;
+    const currency1 = req.body.dropdown1;
+    const currency2 = req.body.dropdown2;
+    let convertedValue = "";
+    //If-blocks to convert currencies accordingly since default base currency is in EUR.
+    if (currency1 === "EUR") {
+        if (currency2 === "EUR") {
+            convertedValue = `${amount} EUR = ${amount} EUR`
+        } else {
+            convertedValue = `${amount} EUR = ${+((amount * content.prices[currency2]).toFixed(2))} ${currency2}`;
+            console.log(convertedValue);
+        }
+    } else if (currency2 !== "EUR") {
+        const currencyToEUR = amount / content.prices[currency1];
+        //Calculate the converted value of the selected base currency. Note the use of unary plus operator '+' used to convert the string value of toFixed() method to a number
+        convertedValue = `${amount} ${currency1} = ${+((currencyToEUR * content.prices[currency2]).toFixed(2))} ${currency2}`
+        console.log(convertedValue);
+    } else {
+        const currencyToEUR = amount / content.prices[currency1];
+        convertedValue = `${amount} ${currency1} = ${+(currencyToEUR.toFixed(2))} ${currency2}`;
+        console.log(convertedValue);
+    }
+
+    res.render("index.ejs", {
+        content: content,
+        conversion: convertedValue,
+        currency1: currency1,
+        currency2: currency2,
+    });
+
+
+})
+
+app.post("/chart", async(req, res) => {
+    const currency1 = req.body.dropdown1;
+    const currency2 = req.body.dropdown2;
+    const chartData = await axios.get(`https://www.frankfurter.app/2021-01-01..?from=${currency1}&to=${currency2}`)
+    console.log(chartData.data);
+
 })
 
 
--- public/index.js
+++ public/index.js
@@ -1,13 +1,24 @@
 function swapOptions() {
     var dropdown1 = document.getElementById("dropdown1");
     var dropdown2 = document.getElementById("dropdown2");
-    var selectedOption = dropdown1.value;     
+    var selectedOption = dropdown1.value;
     dropdown1.value = dropdown2.value;
-    dropdown2.value = selectedOption;        
+    dropdown2.value = selectedOption;
 }
 
 var button = document.querySelector(".swap");
 button.addEventListener("click", swapOptions);
+
+var radioButtons = document.querySelectorAll(".radio-button");
+radioButtons.forEach(function (i) {
+    i.addEventListener("click", function () {
+        radioButtons.forEach(function (j) {
+            j.classList.remove("active");
+        });
+        i.classList.add("active");
+    })
+});
+
 
 // var anchors = document.querySelectorAll('.calculation-nav>a');
 // anchors.forEach(function(anchor) {
